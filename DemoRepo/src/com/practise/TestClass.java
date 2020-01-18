b5d28cc0eb6278c9594e470c2c69d7ca6f57e17d
GiteshSharma <33726201+GiteshSharma@users.noreply.github.com>
2020-01-18 16:16:10 +0530

    Update TestClass.java

--- DemoRepo/src/com/practise/TestClass.java
+++ DemoRepo/src/com/practise/TestClass.java
@@ -4,5 +4,9 @@ public class TestClass {
 	
 	private final String name = "a";
 	private final String named = "c";
+	
+		public TestClass(String abc,String ddd) {
+		System.out.println("abcd");
+	}
 
 }
