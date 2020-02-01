0adf2a4ba9fcdb44356c0bdb4ac8e75af9f480f5
GiteshSharma <33726201+GiteshSharma@users.noreply.github.com>
2020-02-01 17:49:40 +0530

    Update DemoClass.java

--- DemoRepo/src/com/practise/DemoClass.java
+++ DemoRepo/src/com/practise/DemoClass.java
@@ -3,7 +3,9 @@ package com.practise;
 public class DemoClass {
 
 	public static void main(String[] args) {
- System.out.println("Fresh sysout added");
+        System.out.println("Fresh sysout added");
+		
+	System.out.println("Fresh sysout added from master");
 
 
 	}
