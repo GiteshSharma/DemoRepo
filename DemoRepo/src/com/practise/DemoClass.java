1becd4af680b282ac7ce8cf3e5d1c135e8f9c69d
GiteshSharma <33726201+GiteshSharma@users.noreply.github.com>
2020-02-01 16:21:42 +0530

    Update DemoClass.java
    chnagd sys logs

--- DemoRepo/src/com/practise/DemoClass.java
+++ DemoRepo/src/com/practise/DemoClass.java
@@ -3,7 +3,7 @@ package com.practise;
 public class DemoClass {
 
 	public static void main(String[] args) {
-		System.out.println(" Dev Main class");
+		System.out.println("not the Dev Main class");
 
 	}
 
