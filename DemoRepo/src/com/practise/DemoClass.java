43ff8015ab8977cb635c968c25c911710f1cf1ac
GiteshSharma <giteshsharma.14@gmail.com>
2020-02-01 17:21:04 +0530

    fresh sysout

--- DemoRepo/src/com/practise/DemoClass.java
+++ DemoRepo/src/com/practise/DemoClass.java
@@ -3,8 +3,7 @@ package com.practise;
 public class DemoClass {
 
 	public static void main(String[] args) {
-		System.out.println(" Dev Main class");
-		System.out.println(" Looking for recipe");
+ System.out.println("Fresh sysout added");
 
 
 	}
